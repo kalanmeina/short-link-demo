99dfa7309112b4d5c073cd5b858541e92c285bb0
kalanmeina <3035608998@qq.com>
2026-09-14 18:45:57 +0800

    init: 初始化SpringBoot短链接项目基础骨架

---
+++ src/main/java/com/shortlink/shortlinkdemo/ShortLinkDemoApplication.java
@@ -0,0 +1,13 @@
+package com.shortlink.shortlinkdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortLinkDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShortLinkDemoApplication.class, args);
+    }
+
+}
